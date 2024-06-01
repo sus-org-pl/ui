9bed887d20b991062b40590ca42a3f644834a164
Mateusz Rojek <mateusz.rojek@miquido.com>
2024-06-01 19:09:42 +0200

    Updated storybook args types

--- packages/lib/src/Button/Button.stories.ts
+++ packages/lib/src/Button/Button.stories.ts
@@ -1,21 +1,54 @@
 import type { Meta, StoryObj } from "@storybook/svelte";
+
 import Button from "./Button.wc.svelte";
+import {
+  ButtonAsComponent,
+  ButtonColor,
+  ButtonSize,
+  ButtonTabindex,
+  ButtonVariant,
+} from "./Button.types";
 
 const meta = {
-  title: "Buttons",
+  title: "Button",
   component: Button,
+  argTypes: {
+    as: {
+      options: ButtonAsComponent,
+      control: { type: "radio" },
+    },
+    tabindex: {
+      options: ButtonTabindex,
+      control: { type: "select" },
+    },
+    size: {
+      options: ButtonSize,
+      control: { type: "radio" },
+    },
+    color: {
+      options: ButtonColor,
+      control: { type: "radio" },
+    },
+    variant: {
+      options: ButtonVariant,
+      control: { type: "radio" },
+    },
+    iconImageSource: {
+      control: { type: "text" },
+    },
+  },
 } satisfies Meta<Button>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const _Medium_Yellow: Story = {
+export const _Button: Story = {
   args: {
     as: "button",
     size: "medium",
     color: "yellow",
     variant: "filled",
-    label: "Szukaj Susła"
+    label: "Szukaj Susła",
   },
   parameters: {},
 };
--- packages/lib/src/Button/Button.types.ts
+++ packages/lib/src/Button/Button.types.ts
@@ -1,5 +1,11 @@
-export type ButtonAsComponent = "a" | "button";
-export type ButtonTabindex = 0 | -1;
-export type ButtonSize = "small" | "medium" | "large";
-export type ButtonColor = "yellow" | "black" | "blue" | "white";
-export type ButtonVariant = "filled" | "outlined";
+export const ButtonAsComponent = ["a", "button"] as const;
+export const ButtonTabindex = [0, -1] as const;
+export const ButtonSize = ["small", "medium", "large"] as const;
+export const ButtonColor = ["yellow", "black", "blue", "white"] as const;
+export const ButtonVariant = ["filled", "outlined"] as const;
+
+export type ButtonAsComponent = typeof ButtonAsComponent[number];
+export type ButtonTabindex = typeof ButtonTabindex[number];
+export type ButtonSize = typeof ButtonSize[number];
+export type ButtonColor = typeof ButtonColor[number];
+export type ButtonVariant = typeof ButtonVariant[number];
