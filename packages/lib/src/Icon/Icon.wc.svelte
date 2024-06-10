1b0ce34793d7cf4bb7e92832236761f591f2c3b3
Mateusz Rojek <mateusz.rojek@miquido.com>
2024-06-10 21:10:03 +0200

    Ability to set action for icon element

--- packages/lib/src/Icon/Icon.wc.svelte
+++ packages/lib/src/Icon/Icon.wc.svelte
@@ -1,6 +1,9 @@
 <svelte:options customElement="sus-icon" />
 
 <script lang="ts">
+  import cn from "classnames";
+  import type { AriaRole } from "svelte/elements";
+
   import type { IconHeight, IconItem, IconWidth } from "./Icon.types";
   import resources from "./resources";
 
@@ -8,6 +11,19 @@
   export let color: string;
   export let height: IconHeight;
   export let width: IconWidth;
+  export let role: AriaRole = "img";
+
+  const classNames = cn({
+    clickable: role === "button",
+  });
 </script>
 
-{@html resources[item]({ color, height, width })}
+<div on:click class={classNames} {role}>
+  {@html resources[item]({ color, height, width })}
+</div>
+
+<style>
+  .clickable {
+    cursor: pointer;
+  }
+</style>
