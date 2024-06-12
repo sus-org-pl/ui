0db4177ded93a498755a215224c2d3cfba6d0b7e
Mateusz Rojek <mateusz.rojek@miquido.com>
2024-06-12 22:10:35 +0200

    Added social media icons

--- packages/lib/src/Icon/resources/index.ts
+++ packages/lib/src/Icon/resources/index.ts
@@ -4,10 +4,13 @@ import cap from "./_cap";
 import chat from "./_chat";
 import cross from "./_cross";
 import document from "./_document";
+import facebook from "./_facebook";
+import instagram from "./_instagram";
 import lifebelt from "./_lifebelt";
 import megaphone from "./_megaphone";
 import search from "./_search";
 import student from "./_student";
+import twitter from "./_twitter";
 
 const resources = {
   arrow,
@@ -16,10 +19,13 @@ const resources = {
   chat,
   cross,
   document,
+  facebook,
+  instagram,
   lifebelt,
   megaphone,
   search,
   student,
+  twitter,
 };
 
 export default resources;
