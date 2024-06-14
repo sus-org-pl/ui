32c2ef8f9a4845ee145a136dfdec7839d42124fc
Mateusz Rojek <mateusz.rojek@miquido.com>
2024-06-14 22:29:24 +0200

    Added missing imports from the package

--- packages/lib/index.ts
+++ packages/lib/index.ts
@@ -1,3 +1,9 @@
+export { default as Alert } from './src/Alert/Alert.wc.svelte';
 export { default as AppTheme } from './src/AppTheme/AppTheme.wc.svelte';
+export { default as Avatar } from './src/Avatar/Avatar.wc.svelte';
+export { default as Badge } from './src/Badge/Badge.wc.svelte';
 export { default as Button } from './src/Button/Button.wc.svelte';
+export { default as Icon } from './src/Icon/Icon.wc.svelte';
+export { default as Input } from './src/Input/Input.wc.svelte';
+export { default as NewsfeedCard } from './src/NewsfeedCard/NewsfeedCard.wc.svelte';
 export { default as Typography } from './src/Typography/Typography.wc.svelte';
