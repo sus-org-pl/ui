7fa9550fd11c99af7e311baf4c150c76b959e01e
Mateusz Rojek <mateusz.rojek@miquido.com>
2024-06-21 17:14:40 +0200

    Initialized desktop version of navbar

---
+++ packages/lib/src/Navbar/Navbar.stories.ts
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/svelte";
+import Navbar from "./Navbar.wc.svelte";
+
+const meta = {
+  title: "Navbar",
+  component: Navbar,
+  argTypes: {},
+} satisfies Meta<Navbar>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const _Navbar: Story = {
+  args: {},
+  parameters: {
+    backgrounds: {
+      default: "blue-gray",
+    },
+  },
+};
---
+++ packages/lib/src/Navbar/Navbar.wc.svelte
@@ -0,0 +1,35 @@
+<svelte:options customElement="sus-navbar" />
+
+<script>
+  import Logo from "../Logo/Logo.wc.svelte";
+</script>
+
+<nav class="base">
+  <Logo height={38} clickable />
+  <ul class="elementsWrapper">
+        <slot>
+            <li>Link 1</li>
+            <li>Link 2</li>
+            <li>Link 3</li>
+        </slot>
+  </ul>
+</nav>
+
+<style lang="scss">
+  .base {
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    
+    background: var(--white);
+
+    padding: 22px 88px;
+  }
+
+  .elementsWrapper {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 120px;
+  }
+</style>
