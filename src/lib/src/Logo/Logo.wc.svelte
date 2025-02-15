cc4feda40320f5447608f2188c3e5c42bae15b37
Mateusz Rojek <mateusz.rojek@miquido.com>
2025-02-15 12:10:43 +0100

    Added white version of SUS logo

--- src/lib/src/Logo/Logo.wc.svelte
+++ src/lib/src/Logo/Logo.wc.svelte
@@ -2,23 +2,33 @@
 
 <script lang="ts">
   import type { AriaRole } from "svelte/elements";
-  import { LOGO_SVG_ELEMENT } from "./Logo.consts";
+  import { LOGO_SVG_ELEMENT, WHITE_LOGO_SVG_ELEMENT } from "./Logo.consts";
+  import cn from "classnames";
 
   export let height: string | number;
   export let clickable: boolean = false;
+  export let color: "default" | "white" = "default";
 
   const role: AriaRole = clickable ? "button" : "img";
   const width = Math.round(Number(height) * 2.97826086957);
   const as = clickable ? "a" : "div";
   const href = clickable ? "/" : null;
+
+  const classNames = cn(["base"], {
+    white: color === "white",
+  });
 </script>
 
 <svelte:element
   this={as}
   {href}
-  class="base"
+  class={classNames}
   {role}
   aria-label="Stowarzyszenie Umarłych Statutów"
 >
-  {@html LOGO_SVG_ELEMENT({ height, width })}
+  {#if color === "white"}
+    {@html WHITE_LOGO_SVG_ELEMENT({ height, width, color: "white" })}
+  {:else}
+    {@html LOGO_SVG_ELEMENT({ height, width })}
+  {/if}
 </svelte:element>
