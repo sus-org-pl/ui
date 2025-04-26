e976927e50fa0e355d7787d4164dbe01f285ebe6
Mateusz Rojek <mateusz.rojek@miquido.com>
2025-04-26 12:47:25 +0200

    ci: building and deploying web components

---
+++ vite.webcomponents.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
+import packageJson from "./package.json";
+
+export default defineConfig({
+  plugins: [
+    svelte({
+      compilerOptions: {
+        customElement: true,
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      $lib: path.resolve("./src/lib"),
+    },
+  },
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "src/lib/index.ts"),
+      name: "__SUS_WEB_COMPONENTS",
+      formats: ["es"],
+      fileName: () => `web-components-v${packageJson.version}.js`,
+    },
+    outDir: "./web-components-build",
+    emptyOutDir: true,
+    sourcemap: false,
+  },
+});
