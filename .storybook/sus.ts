94eff56e4521ee0c95b3daa76214f54fbcaeeff7
Mateusz Rojek <mateusz.rojek@miquido.com>
2025-06-28 17:59:40 +0200

    build: changed storybook configuration

--- .storybook/sus.ts
+++ .storybook/sus.ts
@@ -4,6 +4,6 @@ export default create({
   base: "light",
   brandTitle: "Stowarzyszenie Umarłych Statutów",
   brandUrl: "https://sus.org.pl/",
-  brandImage: "https://umarlestatuty.pl/wp-content/uploads/2021/02/cropped-SUS_logo_full_color.png",
+  brandImage: "https://umarlestatuty.pl/wp-content/uploads/2025/05/cropped-SUS_logo_full_color.png",
   brandTarget: "_self",
 });
