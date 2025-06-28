76c8d23b0d126599dabfb0739556432fa817d324
Mateusz Rojek <mateusz.rojek@miquido.com>
2025-06-28 18:01:39 +0200

    feat(icons): ability to rotate icons

--- src/lib/src/Icon/Icon.wc.svelte
+++ src/lib/src/Icon/Icon.wc.svelte
@@ -14,15 +14,26 @@
   export let role: AriaRole = "img";
   export let href: string | undefined = undefined;
   export let restProps: Record<string, unknown> = {};
+  export let rotation: "0" | "90" | "180" | "270" = "0";
 
   const classNames = cn({
     clickable: role === "button",
   });
-  
+
+  const rotationStyle = rotation ? `rotate(${rotation}deg)` : undefined;
+
   const as = href ? "a" : "div";
 </script>
 
-<svelte:element this={as} on:click class={classNames} {href} {role} {...restProps}>
+<svelte:element
+  this={as}
+  on:click
+  class={classNames}
+  {href}
+  {role}
+  style={rotationStyle}
+  {...restProps}
+>
   {@html resources[item]({ color, height, width })}
 </svelte:element>
 
