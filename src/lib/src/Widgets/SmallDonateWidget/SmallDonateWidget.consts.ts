fb36153514da2b208f13cad66bbdb5b9b8ae3c33
Mateusz Rojek <mateusz.rojek@miquido.com>
2025-08-30 17:10:22 +0200

    style: changed default values of donate widget

--- src/lib/src/Widgets/SmallDonateWidget/SmallDonateWidget.consts.ts
+++ src/lib/src/Widgets/SmallDonateWidget/SmallDonateWidget.consts.ts
@@ -1,4 +1,4 @@
-export const DEFAULT_DONATE_BODY = "Wesprzyj nas darowizną - pomożesz nam w dalszej walce na rzecz praw uczniów.";
-export const DEFAULT_DONATE_BUTTON_ACTION = "https://nowe.platnosci.ngo.pl/pl/public/campaign/5n7MqG";
+export const DEFAULT_DONATE_BODY = "Wesprzyj nas darowizną – pomożesz nam w dalszej walce na rzecz praw uczniów!";
+export const DEFAULT_DONATE_BUTTON_ACTION = "https://nowe.platnosci.ngo.pl/pl/public/campaign/GdqmZr";
 export const DEFAULT_DONATE_BUTTON_LABEL = "Podaruj darowiznę";
 export const DEFAULT_DONATE_TITLE = "Cenisz naszą działalność?";
